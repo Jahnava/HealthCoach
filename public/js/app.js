187a1f3c8dbeaee5442f60d0485fddf8e13bdd0a
Jahnava <jahnavalmt@gmail.com>
2018-09-06 13:03:47 -0400

    worked on mobile responsiveness

--- public/js/app.js
+++ public/js/app.js
@@ -8,5 +8,7 @@ function magicSparkles(){
   setTimeout(function () {
     $(".animated-text").removeClass("hidden-text");
   }, 600);
-
 }
+
+function AdjustIframeHeightOnLoad() { document.getElementById("form-iframe").style.height = document.getElementById("form-iframe").contentWindow.document.body.scrollHeight + "px"; }
+function AdjustIframeHeight(i) { document.getElementById("form-iframe").style.height = parseInt(i) + "px"; }
